138b1f69bb18df184987ba3aa02939459f2a3d9f
Ana Camila <93491579+annacamila@users.noreply.github.com>
2023-01-28 20:10:35 -0300

    Add files via upload

---
+++ OlaMundo/HelloWord.java
@@ -0,0 +1,11 @@
+package OlaMundo;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Word! Estou comecando meus estudos em Java.");
+
+	}
+
+}
